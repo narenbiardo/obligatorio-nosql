5d010b0475ede60c9204c717088346fb6034c27b
Anthony Mallada <anthonymallada@gmail.com>
2021-10-26 12:58:34 -0300

    Se agrega servicio rest para agregar y listar clientes

--- server.js
+++ server.js
@@ -1,13 +1,58 @@
-r = require('rethinkdb')
-r.connect({ host: 'localhost', port: 28015 }, function(err, conn) {
-  if(err) throw err;
-  r.db('test').tableCreate('tv_shows').run(conn, function(err, res) {
-    if(err) throw err;
-    console.log(res);
-    r.table('tv_shows').insert({ name: 'Star Trek TNG' }).run(conn, function(err, res)
-    {
-      if(err) throw err;
-      console.log(res);
-    });
+var r = require('rethinkdb');
+var express = require('express');
+var app = express();
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+
+// r.connect({ host: 'localhost', port: 28015 }, function(err, conn) {
+//   if(err) throw err;
+//   r.db('test').tableCreate('tv_shows').run(conn, function(err, res) {
+//     if(err) throw err;
+//     console.log(res);
+//     r.table('tv_shows').insert({ name: 'Star Trek TNG' }).run(conn, function(err, res)
+//     {
+//       if(err) throw err;
+//       console.log(res);
+//     });
+//   });
+// });
+
+var p = r.connect({ host: 'localhost', port: 28015, db: 'test'});
+
+app.get("/usuarios", (req, res) => {
+  p.then(function(conn) {
+    r.table('usuarios')
+      .run(conn, function (err, cursor) {
+        if (err) throw err;
+        cursor.toArray(function(err, res2){
+          if (err) throw err;
+          res.json(res2)
+        });
+    })
+  }).error(function(err) {
+    throw err;
   });
+});
+
+app.post("/usuarios", (req, res) => {
+  p.then(function(conn) {
+    r.table('usuarios')
+      .insert({
+        correo: req.body.correo,
+        pass: req.body.pass,
+        nombre: req.body.nombre,
+        apellido: req.body.apellido,
+        roles: []
+    })
+      .run(conn, function (err, res2) {
+        if (err) throw err;
+        res.json(res2);
+    })
+  }).error(function(err) {
+    throw err;
+  });
+});
+
+app.listen(3000, () => {
+  console.log("Corriendo en puerto 3000");
 });
---
+++ usuarios/CUsuarios.js
@@ -0,0 +1,8 @@
+exports.insert = (req, res) => {
+  // let salt = crypto.randomBytes(16).toString('base64');
+  // let hash = crypto.createHmac('sha512', salt)
+  //   .update(req.body.password)
+  //   .digest("base64");
+  // req.body.password = salt + "$" + hash;
+  console.log(req.body.nombre);
+}
