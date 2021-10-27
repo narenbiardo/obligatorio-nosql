e94b534816f517ef6b9d00307d6c6bf9d5c3f4b1
Anthony Mallada <anthonymallada@gmail.com>
2021-10-26 23:42:29 -0300

    Agregar Roles

--- server.js
+++ server.js
@@ -19,9 +19,22 @@ app.use(express.json());
 
 var p = r.connect({ host: 'localhost', port: 28015, db: 'test'});
 
+
+// Obtener codigos de error
+app.get("/errores", (req, res) => {
+  var errores = [
+    'Error 101: al ingresar un nuevo usuario, ya existe un usuario con el correo ingresado',
+    'Error 102: al eliminar un usuario o eliminar roles de un usuario, no existe un usuario con el correo ingresado',
+    'Error 103: al agregar o eliminar uno o más roles de un usuario, al menos uno de los roles ingresados no existen para ese usuario',
+    'Error 104: al agregar o al autenticar a un usuario, la contraseña ingresada es errónea'
+  ]
+  res.json(errores)
+});
+
+
 // Listar usuario
 app.get("/usuarios", (req, res) => {
-  p.then(function(conn) {
+  p.then(function(conBorrarn) {
     r.table('usuarios')
       .run(conn, function (err, cursor) {
         if (err) throw err;
@@ -83,6 +96,28 @@ app.delete('/usuarios:id', (req, res) => {
     throw err;
   });
 });
+
+// Agregar roles
+app.get('/roles', (req, res) => {
+  p.then(function(conn) {
+    r.table('usuarios')
+      .get(req.body.correo)
+      .update({
+        roles: r.row('roles').default([]).setUnion(req.body.roles)})
+      .run(conn, function (err, res2) {
+        if (err) throw err;
+        res.json(res2);
+    })
+  }).error(function(err) {
+    throw err;
+  })
+});
+
+// Eliminar roles
+//
+
+// Autenticar usuario
+//
 
 app.listen(3000, () => {
   console.log("Corriendo en puerto 3000");
