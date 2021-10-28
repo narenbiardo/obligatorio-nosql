80996aaeb0b959d4a565cc03d27f17621fa849b0
Anthony Mallada <anthonymallada@gmail.com>
2021-10-28 13:55:36 -0300

    Se refactorean las funciones para usar sólo consultas de RethinkDB

--- server.js
+++ server.js
@@ -11,69 +11,37 @@ app.get("/errores", (req, res) => {
   var errores = [
     'Error 101: al ingresar un nuevo usuario, ya existe un usuario con el correo ingresado',
     'Error 102: al eliminar un usuario o eliminar roles de un usuario, no existe un usuario con el correo ingresado',
-    'Error 103: al agregar o eliminar uno o más roles de un usuario, al menos uno de los roles ingresados no existen para ese usuario',
-    'Error 104: al agregar o al autenticar a un usuario, la contraseña ingresada es errónea'
+    'Error 103: al eliminar uno o más roles de un usuario, al menos uno de los roles ingresados no existen para ese usuario',
+    'Error 104: al agregar o eliminar roles, o al autenticar a un usuario, la contraseña ingresada es errónea'
   ]
   res.json(errores)
-});
-
-
-// Listar usuarios
-app.get("/usuarios", (req, res) => {
-  p.then(function(conn) {
-    r.table('usuarios')
-      .run(conn, function (err, cursor) {
-        if (err) throw err;
-        cursor.toArray(function(err, res2){
-          if (err) throw err;
-          res.json(res2)
-        });
-    })
-  }).error(function(err) {
-    throw err;
-  });
 });
 
 // Agregar usuario
 app.post("/usuarios", (req, res) => {
   p.then(function(conn) {
-    r.table('usuarios')
-      .insert({
-        correo: req.body.correo,
-        pass: req.body.pass,
-        nombre: req.body.nombre,
-        apellido: req.body.apellido,
-        roles: []
-    })
-      .run(conn, function (err, res2) {
-        if (err) throw err;
-        res.json(res2);
-    })
-  }).error(function(err) {
-    throw err;
-  });
-});
-
-// Detalle usuario
-app.get('/usuarios:id', (req, res) => {
-  p.then(function(conn) {
-    r.table('usuarios')
-      .get(req.body.correo)
-      .run(conn, function (err, res2) {
-        if (err) throw err;
-        res.json(res2);
-    })
-  }).error(function(err) {
-    throw err;
-  });
-});
-
-// Eliminar usuario
-app.delete('/usuarios:id', (req, res) => {
-  p.then(function(conn) {
-    r.table('usuarios')
-      .get(req.body.correo)
-      .delete()
+    r.branch(
+      // if
+      r.table('usuarios')
+        .get(req.body.correo)
+        .eq(null).not(),
+          // then:
+          "Error 101",
+      // else if
+      r.table('usuarios')
+        .get(req.body.correo)('pass')
+        .eq(req.body.pass),
+          // then:
+          "Error 104",
+      // else:
+      r.table('usuarios')
+        .insert({
+          correo: req.body.correo,
+          pass: req.body.pass,
+          nombre: req.body.nombre,
+          apellido: req.body.apellido,
+          roles: []
+        }))
       .run(conn, function (err, res2) {
         if (err) throw err;
         res.json(res2);
@@ -86,10 +54,23 @@ app.delete('/usuarios:id', (req, res) => {
 // Agregar roles
 app.post('/roles:usuarioId', (req, res) => {
   p.then(function(conn) {
-    r.table('usuarios')
-      .get(req.body.correo)
-      .update({
-        roles: r.row('roles').default([]).setUnion(req.body.roles)})
+    r.branch(
+      // if
+      r.table('usuarios')
+        .get(req.body.correo)
+        .eq(null),
+          // then:
+          "Error 102",
+      // else if
+      r.table('usuarios')
+        .get(req.body.correo)('pass')
+        .eq(req.body.pass).not(),
+          // then:
+          "Error 104",
+      // else:
+      r.table('usuarios')
+        .get(req.body.correo)
+        .update({roles: r.row('roles').default([]).setUnion(req.body.roles)}))
       .run(conn, function (err, res2) {
         if (err) throw err;
         res.json(res2);
@@ -104,17 +85,30 @@ app.delete('/roles:usuarioId', (req, res) => {
   p.then(function(conn) {
     var countRolesReq = req.body.roles.length;
     r.branch( // el branch de RethinkDB es una estructura para condicionales
+      // if
       r.table('usuarios')
-      .get(req.body.correo)('roles')
-      .setIntersection(req.body.roles)
-      .count()
-      .eq(countRolesReq), // if, roles que traigo del body request = roles del usuario
-        // hace esto
+        .get(req.body.correo)
+        .eq(null),
+          // then:
+          "Error 102",
+      // else if
+      r.table('usuarios')
+        .get(req.body.correo)('pass')
+        .eq(req.body.pass).not(),
+          // then:
+          "Error 104",
+      // else if, roles que traigo del body request != roles del usuario
+      r.table('usuarios')
+        .get(req.body.correo)('roles')
+        .setIntersection(req.body.roles)
+        .count()
+        .eq(countRolesReq).not(),
+          // then
+          "Error 103",
+        // else:
         r.table('usuarios')
         .get(req.body.correo)
-        .update({roles: r.row('roles').difference(req.body.roles)}),
-      // else, hace esto
-      "Error 104")
+        .update({roles: r.row('roles').difference(req.body.roles)}))
       .run(conn, function (err, res2) {
         if (err) throw err;
         res.json(res2)
@@ -130,3 +124,63 @@ app.delete('/roles:usuarioId', (req, res) => {
 app.listen(3000, () => {
   console.log("Escuchando en puerto 3000");
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+// IGNORAR!!
+//
+//
+// Listar usuarios - NO VA PARA EL OBLIGATORIO
+// app.get("/usuarios", (req, res) => {
+//   p.then(function(conn) {
+//     r.table('usuarios')
+//       .run(conn, function (err, cursor) {
+//         if (err) throw err;
+//         cursor.toArray(function(err, res2){
+//           if (err) throw err;
+//           res.json(res2)
+//         });
+//     })
+//   }).error(function(err) {
+//     throw err;
+//   });
+// });
+
+// // Detalle usuario - NO VA
+// app.get('/usuarios:id', (req, res) => {
+//   p.then(function(conn) {
+//     r.table('usuarios')
+//       .get(req.body.correo)
+//       .run(conn, function (err, res2) {
+//         if (err) throw err;
+//         res.json(res2);
+//     })
+//   }).error(function(err) {
+//     throw err;
+//   });
+// });
+
+// // Eliminar usuario - NO VA
+// app.delete('/usuarios:id', (req, res) => {
+//   p.then(function(conn) {
+//     r.table('usuarios')
+//       .get(req.body.correo)
+//       .delete()
+//       .run(conn, function (err, res2) {
+//         if (err) throw err;
+//         res.json(res2);
+//     })
+//   }).error(function(err) {
+//     throw err;
+//   });
+// });
